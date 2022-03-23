9b1d2a91a6ca8ad803f1434d06c48897d68afd00
yilmazkusatmer <kusatmer@googlemail.com>
2022-03-23 22:27:10 +0100

    reformat code

--- src/main/java/de/yiku/client/RootQueryController.java
+++ src/main/java/de/yiku/client/RootQueryController.java
@@ -5,9 +5,6 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
-
-
-
 @Controller
 public class RootQueryController {
     private final RandomUserClient client;
